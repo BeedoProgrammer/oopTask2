71fb031f7d6d46d4315947da9cffd86134941187
Abdelrahman Hussein <abdelrahmanh38@gmail.com>
2024-08-18 00:04:28 +0300

    Add Child Class Bundle 0

--- bundles.cpp
+++ bundles.cpp
@@ -5,3 +5,15 @@ class Bundle{
     public:
       virtual float calculate_discount(float discount, float price) = 0;
 };
+
+class Bundle_0 : public Bundle{
+    public:
+      Bundle_0(){
+        discount = 1;
+      }
+
+    float calculate_discount(float discount, float price){
+        float discounted_price = price * discount;
+        return discounted_price;
+    }
+};
