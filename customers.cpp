4b3cd800b07d021a2186f7e58e71519aa7676390
asserhanafy <asserhanafy68@gmail.com>
2024-08-18 20:02:29 +0300

    Modify customers.cpp

--- customers.cpp
+++ customers.cpp
@@ -9,9 +9,6 @@ private:
 public:
     customers(long id);
     void countOP();
-    int checkBundle();
-    // Instead of just an integer, it should reference a Bundle object (polymorphic).
-    // This allows the customer to switch between different bundles (e.g., Bundle1, Bundle2, Bundle3) dynamically.
 };
 
 customers::customers(long id)
@@ -23,27 +20,4 @@ customers::customers(long id)
 void customers::countOP()
 {
    this->numofOP++; 
-}
-
-int customers::checkBundle()
-{
-    switch (numofOP)
-    {
-    case 0 ... 2:
-        return 0;
-    case 3 ... 10:
-        return 2;
-    case 11 ... 15:
-        int num;
-        cout << "Would you like to change to Bundle 1?\n1. Yes\n2. No";
-        while(!(cin>>num) || num > 2 || num < 1)
-        {
-            cout<<"Error! Choose from prevoius numbers: ";
-            cin.clear();
-            cin.ignore(100,'\n');
-        }
-        return num;
-    default:
-        return 3;
-    }
 }
