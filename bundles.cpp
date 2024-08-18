63130d0c5070c78192da82375e270cf5a33391d9
Abdelrahman Hussein <abdelrahmanh38@gmail.com>
2024-08-18 21:07:11 +0300

    Add Bundle_3 and Remove discount argument from calculate_discount function in all classes

--- bundles.cpp
+++ bundles.cpp
@@ -1,16 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
 class Bundle_Interface{
     protected:
-      float discount;
+      float discount;  
 
     public:
-      virtual float calculate_discount(float discount, float price) = 0;
+      virtual float calculate_discount(float price) = 0;
+      
 };
 
 class Bundle_0 : public Bundle_Interface{
     public:
-      float calculate_discount(float discount, float price){
+      float calculate_discount(float price){
         return price;
-    }
+      }
 };
 
 class Bundle_1 : public Bundle_Interface{
@@ -19,9 +24,9 @@ class Bundle_1 : public Bundle_Interface{
         discount = 0.15;
       }
 
-    float calculate_discount(float discount, float price){
+      float calculate_discount(float price){
         return price - (price * discount);
-    }
+      }
 };
 
 class Bundle_2 : public Bundle_Interface{
@@ -30,11 +35,41 @@ class Bundle_2 : public Bundle_Interface{
         discount = 100;
       }
 
-    float calculate_discount(float discount, float price){
+      float calculate_discount(float price){
         if(price > 200){
           return price - discount;
         }
         else
           return price;
+      }
+};
+
+class Bundle_3{
+    private:
+      Bundle_1 bun1;
+      Bundle_2 bun2;
+      float x;
+      int num;
+
+    public:
+      float calculate_discount(float price){
+      cout<<"1) Discount by 15% then 100 L,E discount\n";
+      cout<<"2) 100 L,E discount then Discount by 15%\n";
+      cout<<"Which option would you like: ";
+
+      while(!(cin>>num) || num > 2 || num < 1){
+          cout<<"Error! Choose from prevoius numbers: ";
+          cin.clear();
+          cin.ignore(100,'\n');
+        }
+
+        if(num == 1){
+          x = bun1.calculate_discount(price);
+          return  bun2.calculate_discount(x);
+        }
+        else{
+          x = bun2.calculate_discount(price);
+          return  bun1.calculate_discount(x);
+        }
     }
 };
