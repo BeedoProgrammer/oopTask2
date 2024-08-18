3d81da1741bc10b736bd18f99be07e59420a3f33
Anwar <abd.ahm.anwar@gmail.com>
2024-08-18 23:24:20 +0300

    Add store & map & classes

--- main.cpp
+++ main.cpp
@@ -1,75 +1,246 @@
 #include <iostream>
 #include <unordered_map>
 using namespace std;
+unordered_map<long, int> map;
 
-int verifyId(int id){
-  return id==3355?1:0;
+class statBundleNum
+{
+public:
+    int BundleNum(int numofOP)
+    {
+        if ( numofOP < 3)
+        {
+            return 0;
+        }
+        else if (numofOP > 3 && numofOP <= 10)
+        {
+            return 1;
+        }
+        else if (numofOP > 10 && numofOP <= 15)
+        {
+            return 2;
+        }
+        else if (numofOP > 15)
+        {
+            return 3;
+        }
+        return -1;
+    }
+};
+
+class Bundle_Interface
+{
+protected:
+    float discount;
+
+public:
+    virtual float calculate_discount(float price) = 0;
+};
+
+class Bundle_0 : public Bundle_Interface
+{
+public:
+    float calculate_discount(float price)
+    {
+        return price;
+    }
+};
+
+class Bundle_1 : public Bundle_Interface
+{
+public:
+    Bundle_1()
+    {
+        discount = 0.15;
+    }
+
+    float calculate_discount(float price)
+    {
+        return price - (price * discount);
+    }
+};
+
+class Bundle_2 : public Bundle_Interface
+{
+public:
+    Bundle_2()
+    {
+        discount = 100;
+    }
+
+    float calculate_discount(float price)
+    {
+        if (price > 200)
+        {
+            return price - discount;
+        }
+        else
+            return price;
+    }
+};
+
+class Bundle_3
+{
+private:
+    Bundle_1 bun1;
+    Bundle_2 bun2;
+    float x;
+    int num;
+
+public:
+    float calculate_discount(float price)
+    {
+        cout << "1) Discount by 15% then 100 L,E discount\n";
+        cout << "2) 100 L,E discount then Discount by 15%\n";
+        cout << "Which option would you like: ";
+
+        while (!(cin >> num) || num > 2 || num < 1)
+        {
+            cout << "Error! Choose from prevoius numbers: ";
+            cin.clear();
+            cin.ignore(100, '\n');
+        }
+
+        if (num == 1)
+        {
+            x = bun1.calculate_discount(price);
+            return bun2.calculate_discount(x);
+        }
+        else
+        {
+            x = bun2.calculate_discount(price);
+            return bun1.calculate_discount(x);
+        }
+    }
+};
+
+void store(int id) {
+    float Final_price;
+    int bundleNum;
+    if (map.find(id) != map.end()) {
+        int numofOP = map[id]; // Access value in map
+        float price;
+        cout << "Please add price." << endl;
+        cin >> price;
+        statBundleNum S;
+        numofOP++;
+        map[id] = numofOP; 
+        cout << "Number of Operations: " << numofOP << endl;
+        bundleNum = S.BundleNum(numofOP);
+        Bundle_0 bun0;
+        Bundle_1 bun1;
+        Bundle_2 bun2;
+        Bundle_3 bun3;
+        switch (bundleNum)
+    {
+    case 0:
+        
+        Final_price = bun0.calculate_discount(price);
+        break;
+    case 1:
+        Final_price = bun1.calculate_discount(price);
+        break;
+    case 2:
+        
+        Final_price = bun2.calculate_discount(price);
+        break;
+    case 3:
+        
+        Final_price = bun3.calculate_discount(price);
+        break;
+    default:
+        cout << "Invalid bundle number." << endl;
+            return;
+        }
+       
+        cout << "Final Price: " << Final_price << endl;
+        
+        }}
+int verifyId(int id)
+{
+    if (map.find(id) != map.end())
+    {
+        return 1; // ID found
+    }
+    return 0; // ID not found
 }
-void performOperation(){
-int ID;
-cout<<"Enter Customer ID: ";
-cin>>ID;
-if(verifyId(ID)!=1){
-    cout<<"Invalid ID."<<endl;
+
+void performOperation()
+{
+    int ID;
+    cout << "Enter Customer ID: ";
+    cin >> ID;
+    if (verifyId(ID) != 1)
+    {
+        cout << "Invalid ID." << endl;
+    }
+    else
+    {
+        cout << "Valid ID." << endl;
+        // Take op num from Hash map then ask user to mkae opreation
+        store(ID);
+    }
 }
-else{
-    cout<<"Valid ID."<<endl;
-    //Take op num from Hash map then ask user to mkae opreation 
-}
-}
-void store(/*customer c*/){
-    float price;
-    cout<<"Please add price."<<endl;
-    cin >> price;
-    //cont
-}
-void insertmap(unordered_map<long, int>& map, long ID, int numofOP) // Pass by reference
+
+void insertmap(unordered_map<long, int> &map, long ID, int numofOP) // Pass by reference
 {
     map[ID] = numofOP;
 }
 
-void newCustomer(){
+void newCustomer()
+{
     int id;
-    cout<<"Enter Customer ID: ";
-    cin>>id;
-    if(verifyId(id)==1){
-        cout<<"ID already exist."<<endl;
+    cout << "Enter Customer ID: ";
+    cin >> id;
+    if (verifyId(id) == 1)
+    {
+        cout << "ID already exist." << endl;
     }
-    else{
-      //  customers ch(id);
-        
-    unordered_map<long, int> map;
-    insertmap(map, id, 0/* ch.opnumer*/);
-    for (auto x : map) 
-        cout << x.first << " " << x.second << endl;
-
-        cout<<"ID added."<<endl;
+    else
+    {
+        insertmap(map, id, 0);
+        cout << "ID added." << endl;
     }
 }
 
-
-void login(){
+void login()
+{
     int n;
-cout << "Hello to topredo Store!\n"
-     << "1.Add New Customer.\n"
-     <<"2.Perform Operation."<<endl;
-cin>>n;
-switch(n){
+    cout << "\nHello to Topredo Store!\n"
+         << "1.Add New Customer.\n"
+         << "2.Perform Operation.\n"
+         << "3.Exit." << endl;
+    cin >> n;
+    switch (n)
+    {
     case 1:
-    cout<<"Add New Customer."<<endl;
-    newCustomer();
-    break;
+        cout << "Add New Customer." << endl;
+        newCustomer();
+        break;
     case 2:
-    cout<<"Perform Operation."<<endl;
-    performOperation();
-    break;
+        cout << "Perform Operation." << endl;
+        performOperation();
+        break;
+    case 3:
+        cout << "Exit" << endl;
+        for (auto x : map)
+            cout << x.first << " " << x.second << endl;
+        exit(0);
     default:
-    cout<<"Invalid Input."<<endl;
-    break;
+        cout << "Invalid Input." << endl;
+        break;
+    }
+}
+int main()
+{
+    int i = 100;
+    while (i)
+    {
+        login();
+        i--;
+    }
 
-}}
-int main(){
-    login();
-   
+    for (auto x : map)
+        cout << x.first << " " << x.second << endl;
     return 0;
 }
