b508d24467d722a946da164f8dcf228589610362
Anwar <abd.ahm.anwar@gmail.com>
2024-08-18 20:04:19 +0300

    add login to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
 using namespace std;
+void loggin(){
+    int n;
+cout << "Hello to topredo Store!\n----------------------\n"
+     << "1.Add New Customer.\n"
+     <<"2.Perform Operation."<<endl;
+cin>>n;
 
+
+
+}
 int main(){
+    loggin();
     return 0;
 }
